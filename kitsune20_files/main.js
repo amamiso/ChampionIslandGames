e7b9e28f4fad1aa9d5cfd4a85f28f40b633dd6d8
amamiso <amaimiso@outlook.jp>
2025-05-13 21:38:32 +0900

    Add files via upload

---
+++ kitsune20_files/main.js
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = '#FF0000';
+    ctx.fillRect(10, 10, 50, 50);
+});
